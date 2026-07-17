2c65ffde556a2d48a9eafe281e764f13a97a3f97
nobreakchain <82430413+nobreakchain@users.noreply.github.com>
2026-07-17 15:46:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 w.doGoToCoord(0,16);
 w.on("cursorMove", function(e) {
-  var [tileX, tileY, charX, charY] = cursorCoords;
+  let [tileX, tileY, charX, charY] = cursorCoords;
   w.chat.send(charY.toString());
 });
