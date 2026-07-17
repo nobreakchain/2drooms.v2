c51de4a3e1025f443b98bb5d02dcb9dc249c7bcb
nobreakchain <82430413+nobreakchain@users.noreply.github.com>
2026-07-17 16:15:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 w.doGoToCoord(0,16);
+let x = 0;
+let y = 0;
 w.on("cursorMove", function(e) {
-  let [tileX, tileY, charX, charY] = cursorCoords;
-  w.chat.send(charY.toString());
+  let [tileX,tileY,charX,charY] = cursorCoords;
+});
+w.on('writeBefore', function(e) {
+  if(e.TileX >= x*128+62 && e.TileX <= x*128+65 && e.TileY >= y*128-1 && e.TileY <= y*128) {
+    w.chat.send(getChar(x*128+62,y*128+1,0,1));
+  }
 });
