3653a0ecbb58a0ee4dfd5296fcb35b4035bab2bd
nobreakchain <82430413+nobreakchain@users.noreply.github.com>
2026-07-17 16:32:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ w.doGoToCoord(0,16);
 let x = 0;
 let y = 0;
 w.on("cursorMove", function(e) {
+  if(e.tileX >= x*128+61 && e.tileX <= x*128+66 && e.tileY >= y*128-2 && e.tileY <= y*128+1) {} else {
+    // Runs if the condition ISN'T true
+    w.doGoToCoord(y*32,x*32+16);
+  }
   let [tileX,tileY,charX,charY] = cursorCoords;
 });
 w.on('writeBefore', function(e) {
