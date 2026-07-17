603912d23f55267511029434cd335d815ecfa4a9
nobreakchain <82430413+nobreakchain@users.noreply.github.com>
2026-07-17 16:18:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ w.on("cursorMove", function(e) {
   let [tileX,tileY,charX,charY] = cursorCoords;
 });
 w.on('writeBefore', function(e) {
-  if(e.TileX >= x*128+62 && e.TileX <= x*128+65 && e.TileY >= y*128-1 && e.TileY <= y*128) {
+  if(e.tileX >= x*128+62 && e.tileX <= x*128+65 && e.tileY >= y*128-1 && e.tileY <= y*128) {
     w.chat.send(getChar(x*128+62,y*128+1,0,1));
   }
 });
